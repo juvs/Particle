82bd1ac178a26754f289be553a41f2fa9e4585cd
Juvenal Guzman <juvenal.guzman@tenser.mx>
2018-04-15 18:35:36 -0500

    Se incorpora la libreria para control de inclinacion magnetica

---
+++ projects/SmartBit_Metrics/src/A1335Lib.h
@@ -0,0 +1,116 @@
+#include <Wire.h>
+
+const uint8_t start_register = 0x20;
+const uint8_t num_registers = 6;
+const uint8_t start_register2 = 0x34; // Error Mask registers
+const uint8_t num_registers2 = 2;
+
+/**
+ * Content of the registers that should be on every chip
+ * Starts on ANG register (0x20:21)
+ */
+const byte expected_registers[][2] = {
+  {0b00000000, 0b00000000}, // ANG: Excpect RID & EF to be 0
+  {0b10000000, 0b00010001}, // STA: RIDC, Error:0, Status Processing
+  {0b10100000, 0b00000000}, // ERR: RIDC, all errors 0
+  {0b10110000, 0b00000000}, // XERR: RIDC, all errors 0
+  {0b11110000, 0b00000000}, // TSEN: RIDC
+  {0b11100000, 0b00000000}, // FIELD: RIDC
+  {0b11000000, 0b00000000}, // ERM: RIDC & All Errors enabled
+  {0b11010000, 0b00000000}  // XERM: RIDC & All Errors enabled
+};
+
+/**
+ * 1 marks the bits that belong to the expected values
+ * (to check the expected content)
+ */
+const byte expected_registers_mask[][2] = {
+  {0b11000000, 0b00000000}, // ANG: RIDC & EF
+  {0b11110001, 0b11111111}, // STA: RIDC, Error & Status
+  {0b11111100, 0b11111111}, // ERR: All except Interface & CRC Errors
+  {0b11111111, 0b11111111}, // XERR: Check all errors
+  {0b11110000, 0b00000000}, // TSEN: Only RIDC (Register IDentifier Code)
+  {0b11110000, 0b00000000}, // FIELD: RIDC
+  {0b11110100, 0b01111111}, // ERM: RIDC & All Errors except Protocol Erros
+  {0b11111111, 0b11111111}  // XERM: RIDC & All Errors
+};
+
+bool readMemory(uint8_t deviceaddress, uint8_t eeaddress, byte* rdata, uint8_t num);
+bool writeMemory(uint8_t deviceaddress, uint8_t eeaddress, byte* wdata);
+bool writeMemoryCheck(uint8_t deviceaddress, uint8_t eeaddress, byte* wdata);
+
+struct A1335State {
+  uint8_t address;
+  bool isOK;
+  float angle; // in deg
+  uint8_t angle_flags : 2; // error, new
+  uint8_t status_flags : 4;
+  uint16_t err_flags : 12;
+  uint16_t xerr_flags : 12;
+  float temp; // in °C
+  float fieldStrength; // in mT
+
+  byte rawData[8][2];
+};
+
+const uint8_t FLAGS_STRLEN = 10;
+
+bool readDeviceState(uint8_t deviceaddress, A1335State* state);
+bool checkDefaultSettings(A1335State* state);
+bool clearStatusRegisters(uint8_t deviceaddress);
+
+void SerialPrintFlags(uint16_t flags, const char meanings[][FLAGS_STRLEN], uint8_t num);
+void SerialPrintAlignLeft(String s, uint16_t l);
+
+// Number and list of all devices connected
+//const uint8_t all_devices_num_max = 50;
+//A1335State all_devices_state[all_devices_num_max];
+//uint8_t all_devices_num = 0;
+
+const char ANGLE_FLAGS[][FLAGS_STRLEN] = {
+  "NEW",
+  "ERR"
+};
+
+const char STATUS_FLAGS[][FLAGS_STRLEN] = {
+  "ERR",
+  "NEW",
+  "Soft_Rst",
+  "PwON_Rst"
+};
+
+const char ERROR_FLAGS[][FLAGS_STRLEN] = {
+  "MagLow",
+  "MagHigh",
+  "UnderVolt",
+  "OverVolt",
+  "AngleLow",
+  "AngleHigh",
+  "ProcError",
+  "NoRunMode",
+  "(CRC_Err)",
+  "(INTFErr)",
+  "(XOV)",
+  "XERR"
+};
+
+const char XERROR_FLAGS[][FLAGS_STRLEN] = {
+  "SelfTest",
+  "MemAddr",
+  "Execute",
+  "ResetCond",
+  "WTD_Timer",
+  "WTD_Halt",
+  "EEPR_Hard",
+  "SRAM_Hard",
+  "Temp_Err",
+  "AngleWarn",
+  "EEPR_Soft",
+  "SRAM_Soft"
+};
+
+
+
+
+//String commandIn = "";
+//bool line = false;
--- projects/SmartBit_Metrics/src/SmartBit_Metrics.ino
+++ projects/SmartBit_Metrics/src/SmartBit_Metrics.ino
@@ -1,18 +1,78 @@
-/*
- * Project SmartBit_Metrics
- * Description:
- * Author:
- * Date:
- */
+#include "A1335Lib.h"
 
-// setup() runs once, when the device is first turned on.
-void setup() {
-  // Put initialization like pinMode and begin functions here.
+A1335State s;
 
+void setup()
+{
+  Wire.begin();
+
+  Serial.begin(9600);
+  delay(5000);
+  Serial.println("\nI2C Scanner");
 }
 
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  // The core of your code will likely live here.
+int reading = 0;
+float angle;
+int state = LOW;
 
+void loop() {
+
+    if(readDeviceState(0x0C, &s)){
+
+        //Serial.print(F("==== Sensor: "));
+        //Serial.print(s.address);
+        //Serial.print(F(" ==== ("));
+        //Serial.print(s.isOK ? "OK" : "NOT OK");
+        //Serial.println(F(") ===="));
+
+        Serial.print(F("    Angle:  "));
+        Serial.print(s.angle);
+        Serial.print(F("° ("));
+        SerialPrintFlags(s.angle_flags, ANGLE_FLAGS, 2);
+        Serial.println(F(")"));
+
+        /*Serial.print(F("   Status:  "));
+        SerialPrintFlags(s.status_flags, STATUS_FLAGS, 4);
+        Serial.println();
+
+        Serial.print(F("   Errors:  "));
+        SerialPrintFlags(s.err_flags, ERROR_FLAGS, 12);
+        Serial.println();
+
+        Serial.print(F("  XErrors:  "));
+        SerialPrintFlags(s.xerr_flags, XERROR_FLAGS, 12);
+        Serial.println();
+
+        Serial.print(F("     Temp:  "));
+        Serial.print(s.temp);
+        Serial.println(F("°C"));
+
+        Serial.print(F("    Field:  "));
+        Serial.print(s.fieldStrength);
+        Serial.println(F("mT"));*/
+
+
+        /*Serial.println();
+        Serial.println(F("Raw Register Values:"));
+        char buf[30];
+        for(uint8_t i=0; i < 8; i++){
+          // Register numbers
+          uint16_t reg = i < num_registers
+                  ? start_register +(i<<1)
+                  : start_register2 +(i-num_registers)<<1; // (second half)
+          sprintf(buf, "0x%02x: %02x %02x", reg, s.rawData[i][0], s.rawData[i][1]);
+          Serial.println(buf);
+        }*/
+
+        if(s.status_flags & 0b1000){
+            clearStatusRegisters(0x0C);
+            Serial.println(F("Cleared Flags because of Reset Condition; Rescanning..."));
+        }
+
+        //Serial.println();
+        //Serial.println();
+
+    }
+
+    delay(500);
 }
